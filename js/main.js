852518edd7e8f035f179f32684df7be45437bcbd
Adel Abdulazeem <adelnasr029@gmail>
2024-02-27 19:58:25 +0200

    js&css changes

--- js/main.js
+++ js/main.js
@@ -10,7 +10,13 @@ document.querySelector('#clickZone').addEventListener('click', displayCalc)
 document.querySelector('#zero').addEventListener('click', zero)
 document.querySelector('#one').addEventListener('click', one)
 document.querySelector('#two').addEventListener('click', two)
-
+document.querySelector('#three').addEventListener('click', three)
+document.querySelector('#four').addEventListener('click', four)
+document.querySelector('#five').addEventListener('click', five)
+document.querySelector('#six').addEventListener('click', six)
+document.querySelector('#seven').addEventListener('click', seven)
+document.querySelector('#eight').addEventListener('click', eight)
+document.querySelector('#nine').addEventListener('click', nine)
 
 document.querySelector('#plus').addEventListener('click', plus)
 document.querySelector('#minus').addEventListener('click', minus)
@@ -34,12 +40,81 @@ function divide(){
     operator = '/'
 }
 
+function zero(){
+    if(operator == ''){
+        num1 += '0'
+    }
+    else{
+        num2 += '0'
+    }
+}
 function one(){
     if(operator == ''){
         num1 += '1'
     }
     else{
         num2 += '1'
+    }
+}
+function two(){
+    if(operator == ''){
+        num1 += '2'
+    }
+    else{
+        num2 += '2'
+    }
+}function three(){
+    if(operator == ''){
+        num1 += '3'
+    }
+    else{
+        num2 += '3'
+    }
+}function four(){
+    if(operator == ''){
+        num1 += '4'
+    }
+    else{
+        num2 += '4'
+    }
+}function five(){
+    if(operator == ''){
+        num1 += '5'
+    }
+    else{
+        num2 += '5'
+    }
+}
+function six(){
+    if(operator == ''){
+        num1 += '6'
+    }
+    else{
+        num2 += '6'
+    }
+}
+function seven(){
+    if(operator == ''){
+        num1 += '7'
+    }
+    else{
+        num2 += '7'
+    }
+}
+function eight(){
+    if(operator == ''){
+        num1 += '8'
+    }
+    else{
+        num2 += '8'
+    }
+}
+function nine(){
+    if(operator == ''){
+        num1 += '9'
+    }
+    else{
+        num2 += '9'
     }
 }
 
